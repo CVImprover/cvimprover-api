6c7b08e8d561ac0116c80816c4086da04204248e
Atanas Yanev <a.qnev123@gmail.com>
2025-04-15 21:04:41 +0300

    Add JWT auth with endpoints
    Steps to use:
    1. Create user
    2. POST to the token endpoint
    3. receive a refresh and access tokens
    4. Use either to test

--- cvimprover/settings.py
+++ cvimprover/settings.py
@@ -142,6 +142,9 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
--- cvimprover/urls.py
+++ cvimprover/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core.views import ProtectedView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/users/', include('users.urls')),  # assuming you've set up urls in users app
+    path('api/users/protected/', ProtectedView.as_view(), name='protected'),
 ]
