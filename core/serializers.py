c15c883f42dfe1b9effd672a581478eb1a9721a1
Kalin Dimitrov <kalin.dimitrov93@gmail.com>
2025-04-17 20:39:10 +0300

    remove fields that are not needed

--- core/serializers.py
+++ core/serializers.py
@@ -3,14 +3,10 @@ from rest_framework import serializers
 from .models import User
 
 class CustomUserDetailsSerializer(UserDetailsSerializer):
-    phone_number = serializers.CharField(required=False, allow_blank=True)
-    address = serializers.CharField(required=False, allow_blank=True)
     date_of_birth = serializers.DateField(required=False)
 
     class Meta(UserDetailsSerializer.Meta):
         model = User
         fields = UserDetailsSerializer.Meta.fields + (
-            'phone_number',
-            'address',
             'date_of_birth',
         )
