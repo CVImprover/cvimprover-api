fa2f9b147305a42de9f37f570bc1d318cf5ba7f5
Kalin Dimitrov <30594752+DimitrovK@users.noreply.github.com>
2025-05-18 10:20:22 +0300

    Update views.py

--- cvimprover/views.py
+++ cvimprover/views.py
@@ -1,7 +1,7 @@
-# In your Django views.py
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 
 @ensure_csrf_cookie
-def get_csrf_token(request):
-    return JsonResponse({"detail": "CSRF cookie set"})
+def csrf_token_view(request):
+    return JsonResponse({'csrfToken': get_token(request)})
